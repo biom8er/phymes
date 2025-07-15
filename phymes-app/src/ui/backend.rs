0d7f35cd6ad5aa3dba70e5e958832a5f88065886
dmccloskey <dmccloskey87@gmail.com>
2025-07-15 21:54:49 +0200

    fix: android develoment address

--- phymes-app/src/ui/backend.rs
+++ phymes-app/src/ui/backend.rs
@@ -1,4 +1,7 @@
 // Backend URL
 // DM: need to change this to an environmental variable
 //  to better stay in sync with the server url.
+#[cfg(not(target_os = "android"))]
 pub const ADDR_BACKEND: &str = "http://127.0.0.1:4000";
+#[cfg(target_os = "android")]
+pub const ADDR_BACKEND: &str = "http://10.0.2.2:4000";
