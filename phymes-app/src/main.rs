39962b1332a3490cdb3bbcccde3fb326ae7dad09
dmccloskey <dmccloskey87@gmail.com>
2025-07-16 00:42:48 +0200

    wip: testing adding server support directly to dioxus with little luck

--- phymes-app/src/main.rs
+++ phymes-app/src/main.rs
@@ -9,7 +9,21 @@ use ui::main_window::main_window;
 static MAIN_CSS: Asset = asset!("/assets/main.css");
 
 fn main() {
+    #[cfg(any(feature = "web", feature = "mobile", feature = "desktop"))]
     dioxus::launch(app);
+
+    #[cfg(feature = "server")]
+    {
+        use clap::Parser;
+        use phymes_server::server::{server_app::Server, server_config::ServerConfig};
+        tokio::runtime::Runtime::new()
+            .unwrap()
+            .block_on(async move {
+                let config = ServerConfig::parse();
+                Server::new(config).run().await?;
+            }
+        );
+    }
 }
 
 fn app() -> Element {
