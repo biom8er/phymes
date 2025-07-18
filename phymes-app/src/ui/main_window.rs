0cd83c2ec857bdba31299d786101db2a59d2c425
dmccloskey <dmccloskey87@gmail.com>
2025-07-18 18:08:56 +0200

    fix: UI sizing issues on mobile

--- phymes-app/src/ui/main_window.rs
+++ phymes-app/src/ui/main_window.rs
@@ -12,7 +12,7 @@ use super::sign_in::sign_in_modal;
 use super::subjects::subjects_modal;
 use super::svg_icons::{
     database_icon_svg, help_icon_svg, message_icon_svg, person_icon_svg, settings_icon_svg,
-    tools_icon_svg, top_speed_icon_svg,
+    tools_icon_svg, top_speed_icon_svg, search_icon_svg, menu_icon_svg
 };
 use super::tasks::tasks_modal;
 
@@ -50,13 +50,66 @@ impl HeaderMenu {
 #[component]
 pub fn main_window() -> Element {
     let mut header_menu: Signal<HeaderMenu> = use_signal(|| HeaderMenu::Account);
+    let mut navbar_toggle: Signal<bool> = use_signal(|| false);
+
+    use_effect(move || {
+        // Toggle the sidebar visibility
+        let _navbar_toggle = navbar_toggle.read();
+        document::eval(&format!(
+            r#"var elements = document.getElementsByClassName("sidebar");
+            for (var i = 0; i < elements.length; i++) {{
+                var x = elements[i];
+                if (x.style.display === "none") {{
+                    x.style.display = "block";
+                }} else {{
+                    x.style.display = "none";
+                }}
+            }}
+            var elements = document.getElementsByClassName("messaging_list");
+            for (var i = 0; i < elements.length; i++) {{
+                var x = elements[i];
+                if (x.style.marginLeft  === "0px") {{
+                    x.style.display = "64px";
+                }} else {{
+                    x.style.display = "0px";
+                }}
+            }}"#
+        ));
+    });
 
     rsx! {
         main {
             id: "chat_main",
             header {
                 div {
+                    class: "navbar",
+                    label {
+                        class: "checkbtn",
+                        r#for: "navbartoggle",
+                        svg { dangerous_inner_html: menu_icon_svg() }
+                    }
+                    input {
+                        r#type: "checkbox",
+                        id: "navbartoggle",
+                        onclick: move |_| {
+                            let current = navbar_toggle.read().to_owned();
+                            navbar_toggle.set(!current);
+                        },
+                    }
+                }
+                div {
+                    class: "logo",
+                    h1 { id: "logo1", "bio" }
+                    h1 { id: "logo2", "M🤖ER" }
+                }
+                div {
                     class: "search",
+                    button {
+                        onclick: move |_| async move {
+                            header_menu.set(HeaderMenu::Help);
+                        },
+                        svg { dangerous_inner_html: help_icon_svg() }
+                    }
                     // form {
                     //     id: "search_form",
                     //     input {
@@ -66,59 +119,48 @@ pub fn main_window() -> Element {
                     // }
                     // // DM: convert to buttons that actually do something
                     // button { svg { dangerous_inner_html: search_icon_svg() } }
-                    // DM: convert to a responsive navbar that is vertical on desktop and horizontal on mobile
-                    // see https://www.w3schools.com/howto/howto_css_sidebar_responsive.asp
-                    // see https://www.w3schools.com/howto/howto_css_icon_bar.asp
-                    // see https://www.w3schools.com/howto/howto_js_mobile_navbar.asp
-                    // DM: add tooltip for each of the icons
-                    // see https://www.w3schools.com/css/css_tooltip.asp
-                    button {
-                        onclick: move |_| async move {
-                            header_menu.set(HeaderMenu::Help);
-                        },
-                        svg { dangerous_inner_html: help_icon_svg() }
-                    }
-                    button {
-                        onclick: move |_| async move {
-                            header_menu.set(HeaderMenu::Account);
-                        },
-                        svg { dangerous_inner_html: person_icon_svg() }
-                    }
-                    button {
-                        onclick: move |_| async move {
-                            header_menu.set(HeaderMenu::Settings);
-                        },
-                        svg { dangerous_inner_html: settings_icon_svg() }
-                    }
-                    button {
-                        onclick: move |_| async move {
-                            header_menu.set(HeaderMenu::Subjects);
-                        },
-                        svg { dangerous_inner_html: database_icon_svg() }
-                    }
-                    button {
-                        onclick: move |_| async move {
-                            header_menu.set(HeaderMenu::Tasks);
-                        },
-                        svg { dangerous_inner_html: tools_icon_svg() }
-                    }
-                    button {
-                        onclick: move |_| async move {
-                            header_menu.set(HeaderMenu::Message);
-                        },
-                        svg { dangerous_inner_html: message_icon_svg() }
-                    }
-                    button {
-                        onclick: move |_| async move {
-                            header_menu.set(HeaderMenu::Metrics);
-                        },
-                        svg { dangerous_inner_html: top_speed_icon_svg() }
-                    }
                 }
-                div {
-                    class: "logo",
-                    h1 { id: "logo1", "bio" }
-                    h1 { id: "logo2", "M🤖ER" }
+            }
+            
+            div {
+                class: "sidebar",
+                // DM: add tooltip for each of the icons
+                // see https://www.w3schools.com/css/css_tooltip.asp
+                button {
+                    onclick: move |_| async move {
+                        header_menu.set(HeaderMenu::Account);
+                    },
+                    svg { dangerous_inner_html: person_icon_svg() }
+                }
+                button {
+                    onclick: move |_| async move {
+                        header_menu.set(HeaderMenu::Settings);
+                    },
+                    svg { dangerous_inner_html: settings_icon_svg() }
+                }
+                button {
+                    onclick: move |_| async move {
+                        header_menu.set(HeaderMenu::Subjects);
+                    },
+                    svg { dangerous_inner_html: database_icon_svg() }
+                }
+                button {
+                    onclick: move |_| async move {
+                        header_menu.set(HeaderMenu::Tasks);
+                    },
+                    svg { dangerous_inner_html: tools_icon_svg() }
+                }
+                button {
+                    onclick: move |_| async move {
+                        header_menu.set(HeaderMenu::Message);
+                    },
+                    svg { dangerous_inner_html: message_icon_svg() }
+                }
+                button {
+                    onclick: move |_| async move {
+                        header_menu.set(HeaderMenu::Metrics);
+                    },
+                    svg { dangerous_inner_html: top_speed_icon_svg() }
                 }
             }
 
