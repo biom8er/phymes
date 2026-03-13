daf82ce9c73963b03d6a43655fee705e31cc8ddf
dmccloskey <dmccloskey87@gmail.com>
2026-03-13 02:27:28 +0100

    wip: debugging free after use

--- phymes-data/src/external_operators/object_store_processor.rs
+++ phymes-data/src/external_operators/object_store_processor.rs
@@ -7,7 +7,7 @@ use arrow::{array::RecordBatch, datatypes::SchemaRef};
 use bytes::Bytes;
 use chrono::{DateTime, Utc};
 use futures::{FutureExt, Stream, StreamExt};
-use object_store::{GetResult, MultipartUpload, ObjectMeta, ObjectStore, ObjectStoreExt, PutPayload, PutResult, path::Path};
+use object_store::{GetResult, MultipartUpload, ObjectMeta, ObjectStore, ObjectStoreExt, PutPayload, PutResult, WriteMultipart, path::Path};
 use parking_lot::Mutex;
 use phymes_core::{
     AvailableSchemaTrait, AvailableSubjects, BuildableTrait, BuilderTrait, ChunkedWriter, MappableTrait, MessageBuilderTrait, MessageTrait, ObjectStorageBackend, ObjectStorageReader, ObjectStorageWriter, OnChunk, ProcessorTrait, RecordBatchStream, RuntimeEnv, SendableRecordBatchStream, SendableRecordBatchStreamMessage, SendableRecordBatchStreamMessageBuilder, SendableRecordBatchStreamMessageBuilderMap, SendableRecordBatchStreamMessageMap, Table, TableBuilder, TableBuilderTrait, TableTrait, create_attachments_batch, create_bytes_fields, create_chat_record_batch, create_object_store_batch, create_object_store_meta_batch, create_values_fields, make_store, remove_message_by_subject, storage_reader_get_result, storage_reader_stream_result, storage_writer_multipart
@@ -25,12 +25,11 @@ use crate::{
 pub enum ObjectStoreState {
     NotStarted,
     Connecting(Pin<Box<dyn Future<Output = Result<Arc<dyn ObjectStore + 'static>, anyhow::Error>> + Send + 'static>>),
-    StorageReaderGetResult(Pin<Box<dyn Future<Output = Result<GetResult, object_store::Error>> + Send +  'static>>),
-    StorageReaderBytesResult(Pin<Box<dyn Future<Output = Result<Bytes, object_store::Error>> + Send>>),
-    StorageReaderStreamResult(Pin<Box<dyn Stream<Item = Result<Bytes, object_store::Error>> + Send>>),
-    StorageWriterMultipart(Pin<Box<dyn Future<Output = Result<Box<dyn MultipartUpload>, object_store::Error>> + Send  + 'static>>),
-    StorageWriterPutPart(Pin<Box<dyn Future<Output = Result<(), object_store::Error>> + Send>>),
-    StorageWriterComplete(Pin<Box<dyn Future<Output = Result<PutResult, object_store::Error>> + Send  + 'static>>),
+    StorageReaderGetResult((Arc<dyn ObjectStore + 'static>, Path, Pin<Box<dyn Future<Output = Result<GetResult, object_store::Error>> + Send + 'static>>)),
+    StorageReaderBytesResult((Arc<dyn ObjectStore + 'static>, Path, Pin<Box<dyn Future<Output = Result<Bytes, object_store::Error>> + Send + 'static>>)),
+    StorageReaderStreamResult((Arc<dyn ObjectStore + 'static>, Path, Pin<Box<dyn Stream<Item = Result<Bytes, object_store::Error>> + Send + 'static>>)),
+    StorageWriterMultipart((Arc<dyn ObjectStore + 'static>, Path, Pin<Box<dyn Future<Output = Result<Box<dyn MultipartUpload>, object_store::Error>> + Send + 'static>>)),
+    StorageWriterPutResult((Arc<dyn ObjectStore + 'static>, Path, Pin<Box<dyn Future<Output = Result<PutResult, object_store::Error>> + Send + 'static>>)),
     Done,
 }
 
@@ -114,14 +113,6 @@ pub struct ObjectStoreStream {
     record_batches: Option<VecDeque<Map<String, Value>>>,
     /// The location to write the data or location to read the data from
     locations: Option<VecDeque<String>>,
-    /// The connected object store
-    store: Option<Arc<dyn ObjectStore>>,
-    /// Multipart
-    mp: Option<Box<dyn MultipartUpload>>, 
-    /// Pending
-    pending: Option<Arc<Mutex<VecDeque<Vec<u8>>>>>,
-    /// Path
-    path: Option<Path>,
     /// The metadata for the current object
     meta: Option<ObjectMeta>,
 }
@@ -143,10 +134,6 @@ impl ObjectStoreStream {
             state: ObjectStoreState::NotStarted,
             record_batches: None,
             locations: None,
-            store: None,
-            mp: None,
-            pending: None,
-            path: None,
             meta: None,
         })
     }
@@ -248,11 +235,7 @@ impl Stream for ObjectStoreStream {
                 }
 
                 // Create the object store
-                let fut = if let Some(store) = self.store.take() {
-                    Box::pin(async { Ok(store) }) as Pin<Box<dyn Future<Output = Result<Arc<dyn ObjectStore + 'static>, anyhow::Error>> + Send + 'static>>
-                } else {
-                    Box::pin(make_store(self.config.as_ref().unwrap().backend.clone(), self.config.as_ref().unwrap().bucket.clone()))
-                };
+                let fut = Box::pin(make_store(self.config.as_ref().unwrap().backend.clone(), self.config.as_ref().unwrap().bucket.clone()));
 
                 // Update the request state and poll next
                 self.state = ObjectStoreState::Connecting(fut);
@@ -260,7 +243,6 @@ impl Stream for ObjectStoreStream {
             }
             ObjectStoreState::Connecting(fut) => match ready!(fut.as_mut().poll_unpin(cx)) {
                 Ok(store) => {
-                    self.store.replace(store);
 
                     // Get the location prioritizing the subject messages over the config
                     let location = if let Some(batch) = self.record_batches.take() {
@@ -288,18 +270,18 @@ impl Stream for ObjectStoreStream {
                         let err = "Location not provided for ObjectStoreStream.";
                         return Poll::Ready(Some(Err(anyhow!(err))));
                     };
-                    self.path.replace(Path::from(location));
+                    let path = Path::from(location);
 
                     // Determine the opts type
                     match self.config.as_ref().unwrap().ops_type {
                         ObjectStoreOptsType::Get | ObjectStoreOptsType::GetStream | ObjectStoreOptsType::GetMeta => {
-                            let fut = Box::pin(self.store.as_ref().unwrap().get(self.path.as_ref().unwrap()));
-                            self.state = ObjectStoreState::StorageReaderGetResult(fut);
+                            let fut = Box::pin(store.clone().get(&path.clone()));
+                            self.state = ObjectStoreState::StorageReaderGetResult((store, path, fut));
                             self.poll_next(cx)
                         }
                         ObjectStoreOptsType::PutMultipart => {
-                            let fut = Box::pin(self.store.as_ref().unwrap().put_multipart(self.path.as_ref().unwrap()));
-                            self.state = ObjectStoreState::StorageWriterMultipart(fut);
+                            let fut = Box::pin(store.clone().put_multipart(&path.clone()));
+                            self.state = ObjectStoreState::StorageWriterMultipart((store, path, fut));
                             self.poll_next(cx)
                         }
                         ObjectStoreOptsType::Put => {
@@ -319,8 +301,8 @@ impl Stream for ObjectStoreStream {
                             let meta = ObjectMeta { e_tag: None, version: None, location: Path::from(location), last_modified: Utc::now(), size: bytes.len() as u64 };
                             self.meta.replace(meta);
                             let payload = PutPayload::from_bytes(Bytes::from(bytes));
-                            let fut = Box::pin(self.store.as_ref().unwrap().put(self.path.as_ref().unwrap(), payload));
-                            self.state = ObjectStoreState::StorageWriterComplete(fut);
+                            let fut = Box::pin(store.clone().put(&path.clone(), payload));
+                            self.state = ObjectStoreState::StorageWriterPutResult((store, path, fut));
                             self.poll_next(cx)
                         }
                         _ => {
@@ -335,7 +317,7 @@ impl Stream for ObjectStoreStream {
                     Poll::Ready(Some(Err(err)))
                 }
             },
-            ObjectStoreState::StorageReaderGetResult(fut) => match ready!(fut.as_mut().poll_unpin(cx)) {
+            ObjectStoreState::StorageReaderGetResult((store, path, fut)) => match ready!(fut.as_mut().poll_unpin(cx)) {
                 Ok(result) => {
                     match self.config.as_ref().unwrap().ops_type {
                         ObjectStoreOptsType::Get => {
@@ -344,7 +326,7 @@ impl Stream for ObjectStoreStream {
 
                             // Ready the stream for polling
                             let fut = Box::pin(result.bytes());
-                            self.state = ObjectStoreState::StorageReaderBytesResult(fut);
+                            self.state = ObjectStoreState::StorageReaderBytesResult((store.clone(), path.clone(), fut));
                             self.poll_next(cx)
                         }
                         ObjectStoreOptsType::GetStream => {
@@ -353,7 +335,7 @@ impl Stream for ObjectStoreStream {
 
                             // Ready the stream for polling
                             let stream = Box::pin(result.into_stream());
-                            self.state = ObjectStoreState::StorageReaderStreamResult(stream);
+                            self.state = ObjectStoreState::StorageReaderStreamResult((store.clone(), path.clone(), stream));
                             self.poll_next(cx)
                         }
                         ObjectStoreOptsType::GetMeta => {
@@ -403,7 +385,7 @@ impl Stream for ObjectStoreStream {
                     Poll::Ready(Some(Err(err.into())))
                 }
             },
-            ObjectStoreState::StorageReaderBytesResult(fut) => match ready!(fut.as_mut().poll_unpin(cx)) {
+            ObjectStoreState::StorageReaderBytesResult((store, path, fut)) => match ready!(fut.as_mut().poll_unpin(cx)) {
                 Ok(bytes) => {
                     // Initialize the metrics
                     let baseline_metrics =
@@ -448,7 +430,7 @@ impl Stream for ObjectStoreStream {
                     Poll::Ready(Some(Err(err.into())))
                 }
             },
-            ObjectStoreState::StorageReaderStreamResult(fut) => match ready!(fut.as_mut().poll_next_unpin(cx)) {
+            ObjectStoreState::StorageReaderStreamResult((store, path, stream)) => match ready!(stream.as_mut().poll_next_unpin(cx)) {
                 Some(Ok(bytes)) => {
                     // Initialize the metrics
                     let baseline_metrics =
@@ -496,8 +478,8 @@ impl Stream for ObjectStoreStream {
                     self.poll_next(cx)
                 }
             },
-            ObjectStoreState::StorageWriterMultipart(fut) => match ready!(fut.as_mut().poll_unpin(cx)) {
-                Ok(mut mp) => {
+            ObjectStoreState::StorageWriterMultipart((store, path, fut)) => match ready!(fut.as_mut().poll_unpin(cx)) {
+                Ok(mp) => {
                     // Initialize the metrics
                     let baseline_metrics =
                         if let Some(diagnostic_builder) = &self.diagnostic_builder {
@@ -534,79 +516,27 @@ impl Stream for ObjectStoreStream {
                         .to_string();
                     let pending = Arc::new(Mutex::new(VecDeque::new()));
                     let on_chunk = OnChunk::new(&pending);
-                    let chunk_size = self.config.as_ref().unwrap().chunk_size.ok_or(anyhow!("Missing `chunk_size` for multipart put in ObjectStoreStream."))?;
+                    let chunk_size = self.config.as_mut().unwrap().chunk_size.ok_or(anyhow!("Missing `chunk_size` for multipart put in ObjectStoreStream."))?;
                     let mut chunk_writer = ChunkedWriter::new(chunk_size, on_chunk);
                     let size = chunk_writer.write(&bytes)?;
                     let meta = ObjectMeta { e_tag: None, version: None, location: Path::from(location), last_modified: Utc::now(), size: size as u64 };
                     self.meta.replace(meta);
 
-                    // Put the chunk
-                    let chunk = if let Some(chunk) = pending.lock().pop_front() {
-                        Some(chunk)
-                    } else {
-                        None
-                    };
-                    if let Some(chunk) = chunk {
-                        let fut = mp.put_part(Bytes::from(chunk).into());
-                        self.mp.replace(mp);
-                        self.pending.replace(pending);
-                        self.state = ObjectStoreState::StorageWriterPutPart(fut);
-                        self.poll_next(cx)
-                    } else {
-                        let fut = mp.complete();
-                        self.state = ObjectStoreState::StorageWriterComplete(fut);
-                        self.poll_next(cx)
+                    // Put the chunks
+                    let mut write = WriteMultipart::new(mp);
+                    while let Some(chunk) = pending.lock().pop_front() {
+                        write.write(&chunk);
                     }
+                    let fut = write.finish();
+                    self.state = ObjectStoreState::StorageWriterPutResult((store.clone(), path.clone(), Box::pin(fut)));
+                    self.poll_next(cx)
                 }
                 Err(err) => {
                     self.state = ObjectStoreState::Done;
                     Poll::Ready(Some(Err(err.into())))
                 }
             },
-            ObjectStoreState::StorageWriterPutPart(fut) => match ready!(fut.as_mut().poll_unpin(cx)) {
-                Ok(()) => {
-                    // Initialize the metrics
-                    let baseline_metrics =
-                        if let Some(diagnostic_builder) = &self.diagnostic_builder {
-                            Some(
-                                diagnostic_builder
-                                    .clone()
-                                    .to_child("ObjectStoreStream")?
-                                    .baseline_metrics(
-                                        line!(),
-                                        file!(),
-                                        "poll_next.ObjectStoreState::ToBytes",
-                                    ),
-                            )
-                        } else {
-                            None
-                        };
-                    let _timer = baseline_metrics
-                        .as_ref()
-                        .map(|baseline_metrics| baseline_metrics.elapsed_compute().timer());
-
-                    // Put the chunk
-                    let chunk = if let Some(chunk) = self.pending.as_mut().unwrap().lock().pop_front() {
-                        Some(chunk)
-                    } else {
-                        None
-                    };
-                    if let Some(chunk) = chunk {
-                        let fut = self.mp.as_mut().unwrap().put_part(Bytes::from(chunk).into());
-                        self.state = ObjectStoreState::StorageWriterPutPart(fut);
-                        self.poll_next(cx)
-                    } else {
-                        let fut = self.mp.as_mut().unwrap().complete();
-                        self.state = ObjectStoreState::StorageWriterComplete(fut);
-                        self.poll_next(cx)
-                    }
-                }
-                Err(err) => {
-                    self.state = ObjectStoreState::Done;
-                    Poll::Ready(Some(Err(err.into())))
-                }
-            },
-            ObjectStoreState::StorageWriterComplete(fut) => match ready!(fut.as_mut().poll_unpin(cx)) {
+            ObjectStoreState::StorageWriterPutResult((store, path, fut)) => match ready!(fut.as_mut().poll_unpin(cx)) {
                 Ok(results) => {
                     // Initialize the metrics
                     let baseline_metrics =
