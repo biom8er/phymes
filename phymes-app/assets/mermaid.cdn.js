d158f5c07b4993d7ee461f2631c71d40ceadb585
dmccloskey <dmccloskey87@gmail.com>
2026-07-03 21:00:03 +0200

    fix: updated app main to load elk for cdn but embed is still without elk

--- phymes-app/assets/mermaid.cdn.js
+++ phymes-app/assets/mermaid.cdn.js
@@ -2,4 +2,11 @@ import mermaid from 'https://cdn.jsdelivr.net/npm/mermaid@11/dist/mermaid.esm.mi
 import elkLayouts from 'https://cdn.jsdelivr.net/npm/@mermaid-js/layout-elk@0/dist/mermaid-layout-elk.esm.min.mjs';
 window.mermaid = mermaid;
 mermaid.registerLayoutLoaders(elkLayouts);
-mermaid.initialize({theme: "dark", startOnLoad: true });
+mermaid.initialize({
+    theme: "dark",
+    startOnLoad: false,
+    maxTextSize: 50000,
+    maxEdges: 500,
+    securityLevel: "loose",
+    suppressErrorRendering: true
+})
--- phymes-app/assets/mermaid.embed.js
+++ phymes-app/assets/mermaid.embed.js
@@ -1,5 +1,12 @@
 import mermaid from './mermaid.esm.min.mjs';
-import elkLayouts from 'https://cdn.jsdelivr.net/npm/@mermaid-js/layout-elk@0/dist/mermaid-layout-elk.esm.min.mjs';
+import elkLayouts from './mermaid-layout-elk.esm.min.mjs';
 window.mermaid = mermaid;
 mermaid.registerLayoutLoaders(elkLayouts);
-mermaid.initialize({theme: "dark", startOnLoad: true });
+mermaid.initialize({
+    theme: "dark",
+    startOnLoad: false,
+    maxTextSize: 50000,
+    maxEdges: 500,
+    securityLevel: "loose",
+    suppressErrorRendering: true
+})
